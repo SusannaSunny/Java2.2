5f097190ce787ad4f7343e3106e9348e9253d2c0
Susanna <susanna.demkina@gmail.com>
2022-03-20 02:17:04 +0300

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,30 @@ public class Main {
         // параметров программы: начального счёта,
         // суммы пополнения и тп
 
-        int startBalance = 100;
-        int sumBoard = 1000;
-        int sumAdd = 1100;
-        int finBalance;
-        int sumBonus;
+        int startBalance = 100;  // стартовый баланс
+        int sumBoard = 1000;  // Граница бонусов
+        int sumAdd = 1100;  //  Сумма пополнения
+        int finBalance;   //  Итоговый баланс
+        int sumBonus;  //  Количество бонусов
 
         // Условным оператором проверяете превысила ли
         // сумма пополнения порог и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
+            // Проверка суммы пополнения относительно порога
 
         if (sumAdd > sumBoard) {
             finBalance = sumAdd / 100 + startBalance + sumAdd;
         } else {
             finBalance = startBalance + sumAdd;
         }
-
+            // Проверка и расчет бонусов
         if (sumAdd < 1000) {
             sumBonus = 0;
         } else {
             sumBonus = sumAdd / 100;
         }
-
+            // Вывод на экран
         System.out.println("Баланс: " + finBalance);
         System.out.println("Количество бонусов: " + sumBonus);
     }
